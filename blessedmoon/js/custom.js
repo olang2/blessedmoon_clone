31d916e5b916a632de3bcd189b1af812b1c9c5d3
Olang <126643170+olang2@users.noreply.github.com>
2023-04-27 14:25:12 +0900

    Add files via upload

---
+++ blessedmoon/js/custom.js
@@ -0,0 +1,18 @@
+$('.header .nav ul li').mouseenter(function(){
+  $(this).children('.sub1').stop().slideDown()
+})
+$('.header .nav ul li').mouseleave(function(){
+  $(this).children('.sub1').stop().slideUp()
+})
+$('.header .nav ul li').mouseenter(function(){
+  $(this).children('.sub2').stop().slideDown()
+})
+$('.header .nav ul li').mouseleave(function(){
+  $(this).children('.sub2').stop().slideUp()
+})
+$('.header .nav ul li').mouseenter(function(){
+  $(this).children('.sub3').stop().slideDown()
+})
+$('.header .nav ul li').mouseleave(function(){
+  $(this).children('.sub3').stop().slideUp()
+})
